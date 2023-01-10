e7f500313c68871c3ce4c2517b3a8e9eceea9cfe
patipan-patisampita <patipan.patisampita@gmail.com>
2023-01-10 10:48:11 +0700

    update

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'login.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -7,10 +9,52 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Basic Flutter"),
+        title: Text("Home page"),
       ),
       body: Center(
         child: Text("MyApp"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("Elon Mask"),
+              accountEmail: Text("elon@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                // child: Icon(Icons.android),
+                backgroundImage: NetworkImage(
+                    "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg"),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home, color: Colors.indigo, size: 30),
+              title: Text('Home',
+                  style: TextStyle(color: Colors.purple, fontSize: 15)),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomePage(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.login, color: Colors.indigo, size: 30),
+              title: Text('Login',
+                  style: TextStyle(color: Colors.purple, fontSize: 15)),
+              onTap: () {
+                // Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoginPage(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
