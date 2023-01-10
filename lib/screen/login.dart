6f64f78178c6fa62f5f0f258bc52fdd0bceeaddc
patipan-patisampita <patipan.patisampita@gmail.com>
2023-01-10 11:55:23 +0700

    update

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController nameController = TextEditingController();
+  final _formkey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login page"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Form(
+          key:_formkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                validator: (value) {
+                  if(value == ""){
+                    return "Field is required";
+                  }
+                  return null;
+                },
+                controller: nameController,
+                decoration: InputDecoration(
+                  labelText: "Name",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              ElevatedButton(onPressed: () {
+                if(_formkey.currentState!.validate() == true){
+                  print("Form Submitted Successfully");
+                }
+              }, child: Text("Login"))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
